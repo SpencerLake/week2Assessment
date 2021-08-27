e92dbb000e7da1af8577ebda38c260161a1bf497
Spencer-Lake <spencer.m.lake11@gmail.com>
2021-08-27 10:19:01 -0600

    finished gitInfo

--- gitInfo.js
+++ gitInfo.js
@@ -11,6 +11,8 @@
 
 //CODE HERE
 
+const gitDefinition = 'Git is a a versatile control system that handles information'
+
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable called 'gitHubDefinition'.  
@@ -18,6 +20,8 @@
 */
 
 //CODE HERE
+
+const gitHubDefinition = 'GitHub is a web browser where you can access and look at the information that you have stored on Git.'
 
 //////////////////PROBLEMS 3 - 8////////////////////
 /*
@@ -35,6 +39,11 @@
 
 //CODE HERE
 
+const init = {
+    description: `A command to initialize a Git repository under the file you're working in` ,
+    code: 'git init'
+}
+
 //////////////////PROBLEM 4////////////////////
 /*
     Create an object called 'clone' with 'description' and 'code' properties 
@@ -42,6 +51,11 @@
 */
 
 //CODE HERE
+
+const clone = {
+    decription: `A command to create a copy of a repository in a new location`,
+    code: `git clone`
+}
 
 //////////////////PROBLEM 5////////////////////
 /*
@@ -51,6 +65,11 @@
 
 //CODE HERE
 
+const status = {
+    description: `A command that allows you to see the status of the file you are located in, to see if it being tracked, what has been staged to push and what hasn't`,
+    code: `git status`
+}
+
 //////////////////PROBLEM 6////////////////////
 /*
     Create an object called 'add' with 'description' and 'code' properties 
@@ -58,6 +77,11 @@
 */
 
 //CODE HERE
+
+const add = {
+    description: `A command that adds all the changes to a staging area before getting committed`,
+    code: `"git add ." or "git add 'fileName'`
+}
 
 //////////////////PROBLEM 7////////////////////
 /*
@@ -67,6 +91,10 @@
 
 //CODE HERE
 
+const commit = {
+    description: `A command that commits the added information to be pushed to the repository`,
+    code: `git commit -m "fileName"`
+}
 
 //////////////////PROBLEM 8////////////////////
 /*
@@ -75,3 +103,8 @@
 */
 
 //CODE HERE
+
+const push = {
+    description: 'A command that pushed all the committed changed to the repository',
+    code: 'git push'
+}
